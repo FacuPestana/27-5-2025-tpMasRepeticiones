82ef252bea36bcde99cf62005ece4836c3f6099f
alumno <alumno@Instituto.local>
2025-05-28 11:20:43 -0300

    push
    push

---
+++ script.js
@@ -0,0 +1,41 @@
+//1 
+let animales = ["perro", "gato", "conejo", "loro"];
+
+for (let i = 0; i < animales.length; i++) {
+    console.log(animales[i]);
+    i++;
+}
+
+//2
+for (let i = 0; i < animales.length; i++) {
+  if (i % 2 === 0) {
+    console.log(animales[i]);
+  }
+}
+
+//3
+let numeros = [4, 7, 2, 9, 5];
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];
+}
+
+console.log("La suma es " + suma);
+
+//4
+let datos = [3, 6, 1, 8];
+let resultado = [];
+
+for (let i = 0; i < datos.length; i++) {
+  resultado.push(datos[i] * 2);
+}
+
+console.log(resultado);
+
+//5
+
+let contador = 0 
+let busqueda = nombr
+//6
+let valores = [5, 0, 8, 0, 2];
